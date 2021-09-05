b873c9a31e6816c2f6b2d98250d92095726b7126
lizuncong <2900170896@qq.com>
2021-09-05 15:41:50 +0800

    stash

--- my-webpack/cli.js
+++ my-webpack/cli.js
@@ -1,5 +1,8 @@
-const webpack = require('./webpack-lib/index.js')
+// const webpack = require('./webpack-lib/index.js')
+const webpack = require('webpack')
 const options = require('./webpack.config')
+debugger;
+
 const compiler = webpack(options)
 compiler.run((err, stat) => {
     console.log('stat...', stat)
