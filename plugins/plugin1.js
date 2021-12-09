6d27d9f93ec65bfc7cd40c04bc0ce64f21b3ad88
lizuncong <2900170896@qq.com>
2021-12-09 23:45:07 +0800

    stash

--- plugins/plugin1.js
+++ plugins/plugin1.js
@@ -2,12 +2,11 @@
 class MyPlugin1 {
     constructor(){
         console.log('MyPlugin1  construct=====')
-        debugger;
     }
     apply(compiler){
         console.log('Myplugin1 apply======')
         compiler.hooks.emit.tapPromise('MediaExtractPlugin', async (compilation) => {
-            debugger;
+            console.log();
         })
     }
 }
