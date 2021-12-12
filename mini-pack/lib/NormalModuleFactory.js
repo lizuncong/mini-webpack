c56fc84fdbbd88111186de977a8baed3a83db3e2
lizuncong <2900170896@qq.com>
2021-12-12 23:30:56 +0800

    stash

--- mini-pack/lib/NormalModuleFactory.js
+++ mini-pack/lib/NormalModuleFactory.js
@@ -12,9 +12,14 @@
         this.context = context || "";
         this.resolverFactory = resolverFactory;
         this.ruleSet = new RuleSet(options.defaultRules.concat(options.rules));
-        console.log('NormalModuleFactory constructor====')
+        console.log('NormalModuleFactory constructor====', this.ruleSet)
      }
      create(data, callback){
+		const dependencies = data.dependencies;
+        const context = data.context || this.context;
+        const resolveOptions = data.resolveOptions || {};
+        const request = dependencies[0].request;
+		const contextInfo = data.contextInfo || {};
         console.log('NormalModuleFactory.create====', data)
      }
  
--- mini-pack/lib/RuleSet.js
+++ mini-pack/lib/RuleSet.js
@@ -4,10 +4,26 @@ module.exports = class RuleSet {
         const ident = "ref-"
         this.rules = rules.map((rule, idx) => {
             const id = `${ident}-${idx}`
-            const newRule = {...rule};
-            if(rule.test){
-                newRule.resource = {}
+            const newRule = {};
+            const condition = rule.test;
+            if(condition){
+                newRule.resource = condition.test.bind(condition)
             }
+            const item = rule.use;
+            if(item){
+                const newItem = {}
+                Object.keys(item).filter(function(key) {
+                    return !["options", "query"].includes(key);
+                }).forEach(key => {
+                    newItem[key] = item[key]
+                })
+                newRule.use = [newItem]
+            }
+            Object.keys(rule).filter(key => {
+                return !['test'].includes(key)
+            }).forEach(key => {
+                newRule[key] = rule[key]
+            })
             return newRule;
         })
     }
