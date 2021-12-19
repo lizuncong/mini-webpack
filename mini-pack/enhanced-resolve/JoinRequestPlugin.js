89a1be0fd9f2cec56c337c08ba61b0a6b0cbc421
lizuncong <2900170896@qq.com>
2021-12-19 23:41:29 +0800

    stash

--- mini-pack/enhanced-resolve/JoinRequestPlugin.js
+++ mini-pack/enhanced-resolve/JoinRequestPlugin.js
@@ -11,6 +11,7 @@ module.exports = class JoinRequestPlugin {
 		resolver
 			.getHook(this.source)
 			.tapAsync("JoinRequestPlugin", (request, resolveContext, callback) => {
+				console.log('JoinRequestPlugin==', path.join(request.path, request.request))
 				const obj = Object.assign({}, request, {
 					// webpack使用的是resolver.join，其实就是memory-fs/lib下的join方法
 					path: path.join(request.path, request.request),
