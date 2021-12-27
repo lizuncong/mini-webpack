816b04f623bcaea13c6d0abd96db1f964be10c18
lizuncong <2900170896@qq.com>
2021-12-27 23:31:43 +0800

    stash

--- mini-pack/lib/JavascriptGenerator.js
+++ mini-pack/lib/JavascriptGenerator.js
@@ -8,8 +8,42 @@ class JavascriptGenerator {
     generate(module, dependencyTemplates, runtimeTemplate){
         const originalSource = module.originalSource();
         const source = new ReplaceSource(originalSource);
+        this.sourceBlock(
+			module,
+			module,
+			[],
+			dependencyTemplates,
+			source,
+			runtimeTemplate
+		);
         return source
     }
+    sourceBlock(
+		module,
+		block,
+		availableVars,
+		dependencyTemplates,
+		source,
+		runtimeTemplate
+	) {
+		for (const dependency of block.dependencies) {
+			this.sourceDependency(
+				dependency,
+				dependencyTemplates,
+				source,
+				runtimeTemplate
+			);
+		}
+	}
+    sourceDependency(dependency, dependencyTemplates, source, runtimeTemplate) {
+		const template = dependencyTemplates.get(dependency.constructor);
+		if (!template) {
+			throw new Error(
+				"No template for dependency: " + dependency.constructor.name
+			);
+		}
+		template.apply(dependency, source, runtimeTemplate, dependencyTemplates);
+	}
 }
 
 module.exports = JavascriptGenerator;
